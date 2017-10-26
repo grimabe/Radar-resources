c3e75a6fdda1c74a827c14561781926b3b324865
Benjamin Grima <benjamin@usabilla.com>
2017-10-26 16:10:53 +0200

    Add resources for openradar 6718346540089344

---
+++ openradar-6718346540089344/DemoFramework/DemoFramework/MyDemoFramework.swift
@@ -0,0 +1,15 @@
+//
+//  MyDemoFramework.swift
+//  DemoFramework
+//
+//  Created by Benjamin Grima on 24/10/2017.
+//  Copyright © 2017 Demo. All rights reserved.
+//
+
+import Foundation
+
+public class MyDemoFramework {
+    public class func printSomething() {
+        print("Here I am")
+    }
+}
